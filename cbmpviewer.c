568a802a9f3837086f0126be4f9901eb4ee1004f
Kotaro Terada <cq.cq.de.arjgk@gmail.com>
2012-11-06 01:49:23 +0900

    2値化の数値で表示するところまで

--- cbmpviewer.c
+++ cbmpviewer.c
@@ -6,16 +6,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
 #include "cbmpviewer.h"
+
+#define MAX(a,b) (((a) > (b)) ? (a) : (b))
+#define MIN(a,b) (((a) < (b)) ? (a) : (b))
+
+// RBGの2値化のしきい値
+// 0~255までで表されているものを2値化するときどこを境にするか
+#define THRESHOLD_R 128
+#define THRESHOLD_G 128
+#define THRESHOLD_B 128
 
 // メイン関数
 int main(int argc, char *argv[]) {
+
     // 引数チェック
     if (argc != 2) {
         usage();
         return EXIT_SUCCESS;
     }
     // argv[1]: 画像ファイル名
+
+    // 画面クリア
+    //printf("\x1b[2J");
 
     // Viewプロシージャへ
     viewproc(argv[1]);
@@ -35,7 +50,10 @@ void viewproc(char *filename) {
     bmpfileheader_t fh;
     bmpinfoheader_t ih;
     pixel_t **pix;
-    int i;
+    uint32_t i, j, m, n;
+    struct winsize win; // コンソールサイズ
+    uint32_t bpl_c, bpl_r; // コンソール文字に対するbmpのピクセル比率
+    uint32_t letter, line; // コンソール出力の1行の文字数と行数
 
     // 画像ファイルオープン
     if ((fp = fopen(filename, "rb")) == NULL) {
@@ -78,10 +96,62 @@ void viewproc(char *filename) {
     printf("[READDATA: OK]\n");
 #endif
 #ifdef DEBUG
-    showbmpdata(pix, ih.width, ih.height);
+    //showbmpdata(pix, ih.width, ih.height);
 #endif
 
-    
+    // ファイルクローズ
+    fclose(fp);
+#ifdef DEBUG
+    printf("[FILECLOSE: OK]\n");
+#endif
+
+    // コンソールサイズ取得
+    ioctl(STDOUT_FILENO, TIOCGWINSZ, &win);
+#ifdef DEBUG
+    printf("[CONSOLESIZE: OK] col=%u,row=%u\n", win.ws_col, win.ws_row);
+#endif
+
+    // コンソール文字とピクセル比率の決定
+    // ピクセル比率とはbmpでの何ピクセルがコンソールでの1文字になるか
+    // 符号無し整数値で最小値は1
+    // コンソールでの文字は縦横比が2:1になることも注意
+    // 参照: pixel_letter.example
+    bpl_c = MAX(ih.width / win.ws_col, 1);
+    bpl_r = MAX(ih.height / win.ws_row, 1);
+    if (bpl_c < bpl_r) bpl_c = bpl_r >> 1; // /2
+    else               bpl_r = bpl_c << 1; // *2
+    letter = ih.width / bpl_c;
+    line = ih.height / bpl_r;
+    printf("[BMP/LETTER: OK] bpl_c=%u,bpl_r=%u,letter=%u,line=%u\n", bpl_c, bpl_r, letter, line);
+
+    // 色変換と出力
+    for (i = 0; i < line; i++) {
+        for (j = 0; j < letter; j++) {
+            uint32_t r, g, b, s;
+            uint8_t clr;
+
+            // 1文字で表される文のピクセルのRGB値の平均を求める
+            // まずはsum
+            r = g = b = 0;
+            for (m = 0; m < bpl_r; m++) {
+                for (n = 0; n < bpl_c; n++) {
+                    r += pix[i * bpl_r + m][j * bpl_c + n].red;
+                    g += pix[i * bpl_r + m][j * bpl_c + n].green;
+                    b += pix[i * bpl_r + m][j * bpl_c + n].blue;
+                }
+            }
+            // 平均
+            s = bpl_r * bpl_c;
+            r = r / s;
+            g = g / s;
+            b = b / s;
+
+            // RGB各値を2bit化して、RGBを3bit(8通り)で表す
+            clr = (r / THRESHOLD_R << 2) + (g / THRESHOLD_G << 1) + b / THRESHOLD_B;
+            printf("%u", clr);
+        }
+        printf("\n");
+    }
 
     // メモリ解放
     for (i = 0; i < ih.height; i++) {
@@ -90,12 +160,6 @@ void viewproc(char *filename) {
     free(pix);
 #ifdef DEBUG
     printf("[MEMORYFREE: OK]\n");
-#endif
-
-    // ファイルクローズ
-    fclose(fp);
-#ifdef DEBUG
-    printf("[FILECLOSE: OK]\n");
 #endif
 }
 
@@ -136,6 +200,11 @@ void freadwitherror(void *buf, int byte, FILE *fp) {
 
 // 画像ヘッダの(このプログラムで対応する)フォーマットチェック
 void checkbmpheader(bmpfileheader_t *fh, bmpinfoheader_t *ih) {
+    // 高さが負数だったらはじく
+    if (ih->height < 0) {
+        printf("Error: this program currently does not work for top-down bmp\n");
+        exit(EXIT_SUCCESS);
+    }
     // ビットカウント24以外ははじく
     if (ih->bitcount != 24) {
         printf("Error: this program currently does not work for %u-bit bmp\n", ih->bitcount);
@@ -156,7 +225,7 @@ void showbmpheader(bmpfileheader_t *fh, bmpinfoheader_t *ih) {
     printf("reserved1: %u\n", fh->reserved1);
     printf("reserved1: %u\n", fh->reserved2);
     printf("offbits: %u\n", fh->offbits);
-    printf("----");
+    //printf("----\n");
     printf("--info header--\n");
     printf("size: %u\n", ih->size);
     printf("width: %d\n", ih->width);
@@ -169,14 +238,15 @@ void showbmpheader(bmpfileheader_t *fh, bmpinfoheader_t *ih) {
     printf("ypixpermeter: %d\n", ih->ypixpermeter);
     printf("clrused: %u\n", ih->clrused);
     printf("clrimporant: %u\n", ih->clrimporant);
-    printf("----\n");
+    //printf("----\n");
 }
 
 // 画像データをメモリに読み込む
+// データはボトムアップに入っていることに注意する
 void readbmpdata(FILE *fp, pixel_t **pix, int32_t w, int32_t h) {
     int i, j;
 
-    for (i = 0; i < h; i++) {
+    for (i = h - 1; i >= 0; i--) {
         for (j = 0; j < w; j++) {
             // B G R の順
             freadwitherror(&pix[i][j].blue, 1, fp);
